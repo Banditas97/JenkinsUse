f2a2ccbb03c3d8bb28af17a036a6a645bf7a9982
bandita <bandita@Barsha>
2024-08-07 02:15:38 +0530

    NewProject I commited..

---
+++ src/test/java/New/PracticeForJen.java
@@ -0,0 +1,22 @@
+package New;
+
+import org.testng.annotations.Test;
+
+public class PracticeForJen 
+	{
+	@Test
+	public void fst()
+	{
+		System.out.println("This is the firste one");
+	}
+	@Test
+	public void second()
+	{
+		System.out.println("This is the second one");
+	}
+	@Test
+	public void third()
+	{
+		System.out.println("This is the third one");
+	}
+	}
